0b01120797c9d2e7164266b15a34212272fec97c
Pooja-p18 <poojaupadhya96@gmail.com>
2026-08-13 15:47:21 +0530

    Practice Python list basics

---
+++ python/01_create_list.py
@@ -0,0 +1,14 @@
+thislist = ["apple","banana","cherry","mango","kiwi","orange"]
+print(thislist)
+print(len(thislist))
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(type(list1))
+print(type(list2))
+print(type(list3))
+
+thislist = list(("apple", "banana", "kiwi", "cherry"))
+print(thislist)
