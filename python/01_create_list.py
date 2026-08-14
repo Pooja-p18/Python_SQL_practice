564f3705f26c744ec3e0c8012a6f5c2bee66cb5c
Pooja-p18 <poojaupadhya96@gmail.com>
2026-08-14 14:17:56 +0530

    Practice changing Python list items

--- python/01_create_list.py
+++ python/01_create_list.py
@@ -12,3 +12,64 @@ print(type(list3))
 
 thislist = list(("apple", "banana", "kiwi", "cherry"))
 print(thislist)
+
+
+#Accessing the list items
+thislist = ["apple", "banana", "cherry","mango", "kiwi", "orange"]
+print(thislist[3])
+
+#negative indexing
+thislist = ["apple", "banana", "cherry","mango", "kiwi", "orange"]
+print(thislist[-2])
+
+#ranges of indexes
+thislist = ["apple", "banana", "cherry","mango", "kiwi", "orange"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry","mango", "kiwi", "orange"]
+print(thislist[:4])
+
+thislist = ["apple", "banana", "cherry","mango", "kiwi", "orange"]
+print(thislist[2:])
+
+thislist = ["apple", "banana", "cherry","mango", "kiwi", "orange"]
+print(thislist[-4:-1])
+
+thislist = ["apple", "banana", "cherry","mango", "kiwi", "orange"]
+if "papaya" in thislist:
+    print("Yes, 'papaya' is in the fruits list")
+else:
+    print("No, 'papaya' is not in the fruits list")
+    
+    
+# -------------------------
+# Changing List Items
+# -------------------------
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+
+
+
+
+
+
+
+
