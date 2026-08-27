b77dd6278b66bc0e173d8aaa68992fb5927589bc
Pooja-p18 <poojaupadhya96@gmail.com>
2026-08-27 14:39:01 +0530

    Add Python tuple exercises

--- python/02_tuples.py
+++ python/02_tuples.py
@@ -105,3 +105,13 @@ i = 0
 while i < len(thistuple):
     print(thistuple[i])
     i = i + 1
+    
+#join tuples
+tuple1 = ("a", "b", "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+fruits= ("apple", "kiwi", "orange")
+mytuple= fruits*2
+print(mytuple)
