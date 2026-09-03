d54775c554fca26b622bd5fa75e161608076db7c
Pooja-p18 <poojaupadhya96@gmail.com>
2026-09-03 10:12:40 +0530

    Added recent modified dictionaries and ifelse in python

---
+++ python/05_ifelse.py
@@ -0,0 +1,20 @@
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+    
+n = 15
+if n > 0:
+    print("The number is positive")
+
+#multiple statements in a block
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+  
+#using variable in condition
+is_logged_in = True
+if is_logged_in:
+    print("Welcome back!")
