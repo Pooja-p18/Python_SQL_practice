ffdc8f83b465e69514d26f778aebabe402c0b78c
Pooja-p18 <poojaupadhya96@gmail.com>
2026-09-05 15:48:24 +0530

    Added elif and shorthand operations

--- python/05_ifelse.py
+++ python/05_ifelse.py
@@ -62,3 +62,76 @@ elif day == 6:
   print("Saturday")
 elif day == 7:
   print("Sunday")
+  
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+    
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  
+number = 7
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+    
+temperature = 22
+if temperature > 30:
+    print("Its hot outside!")
+elif temperature > 20:
+    print("Its warm outside")
+elif temperature > 10:
+    print("Its cool outside")
+else:
+    print("Its cold outside!")
+    
+    
+username = "Emil"
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+  
+#shorthand If
+a = 5
+b = 2
+if a > b:
+    print("a is greater than b")
+    
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+x = 15
+y = 20
+max_value = x if x > y else y
+print("Maximum value:", max_value)
+
+username = ""
+display_name = username if username else "Guest"
+print("Welcome,", display_name)
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
