40b1687df8ca0deef31adbc8e4170bd750c45769
Pooja-p18 <poojaupadhya96@gmail.com>
2026-09-11 11:55:01 +0530

    Added scope, lambda and recursion exercises

--- python/07_functions.py
+++ python/07_functions.py
@@ -230,3 +230,87 @@ def outer():
     print("Outer:", x)
 outer()
 print("Global:", x)
+
+#Lambda functions
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))  
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+def myfunc(n):
+    return lambda a : a * n
+mydoubler = myfunc(2)
+print(mydoubler(11))
+
+def myfunc(n):
+    return lambda a : a * n
+mytripler = myfunc(3)
+print(mytripler(11))
+
+def myfunc(n):
+    return lambda a : a * n
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+print(mydoubler(11))
+print(mytripler(11))
+
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x : x * 2, numbers))
+print(doubled)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+print(odd_numbers)
+
+students = [("Emil", 25), ("Tobias", 22), ("Linus", 28)]
+sorted_students = sorted(students, key=lambda x : x[1])
+print(sorted_students)
+
+words = ["apple", "pie", "banana", "cherry"]
+sorted_words = sorted(words, key=lambda x : len(x))
+print(sorted_words)
+
+#Recursion
+def countdown(n):
+    if n <= 0:
+        print("Done!")
+    else:
+        print(n)
+        countdown(n - 1)
+countdown(5)
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+print(factorial(5))
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+print(fibonacci(7))
+
+def sum_list(numbers):
+    if len(numbers) == 0:
+        return 0
+    else:
+        return numbers[0] + sum_list(numbers[1:])
+my_list = [1, 2, 3, 4, 5]
+print(sum_list(my_list))
+
+def find_max(numbers):
+    if len(numbers) == 1:
+        return numbers[0]
+    else:
+        max_of_rest = find_max(numbers[1:])
+        return numbers[0] if numbers[0] > max_of_rest else max_of_rest
+my_list = [3, 7, 2, 9, 1]
+print(find_max(my_list))
+    
